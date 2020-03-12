3c9a924d735de0411d9a09925de716b912b6b76e
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-12 21:45:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,33 +1,36 @@
 # Creating a method that takes a string arg
-#def reverse_each_word(wrds)
-#  
-#  # creates an array from the motified wrds
-#  rvs_wrds = []
-#  
-#  # Creates an array from the string...
-#  sm_strng = wrds.split(" ")
-#  
-#  # Reverses each elements value
-#  sm_strng.each do |wrd|
-#    
-#    #stores them in an array
-#    rvs_wrds << wrd.reverse  
-#  end
-#  
-#  #returns and joins our modified wrds with a space between them
-#  return rvs_wrds.join(" ")
-#
-#end
+def reverse_each_word(wrds)
+  
+  # creates an array from the motified wrds
+  rvs_wrds = []
+  
+  # Creates an array from the string...
+  sm_strng = wrds.split(" ")
+  
+  # Reverses each elements value
+  sm_strng.each do |wrd|
+    
+    #stores them in an array
+    rvs_wrds << wrd.reverse  
+  end
+  
+  #returns and joins our modified wrds with a space between them
+  return rvs_wrds.join(" ")
+
+end
 
 # Create a method that takes a string arg
 def reverse_wrds(wrds)
 
   rvs_wrds2 = wrds.split(" ")
 
-  rvs_wrds2.collect do |wrd|
+  rvs_wrds2.map do |wrd|
     (wrd).reverse!
   end
 
   print rvs_wrds2.join(" ")
 
 end
+
+
+reverse_wrds("Does this thing even work?")
