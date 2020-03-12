69de2e1fdf6e0e320ff09e4467925dec6903024a
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-12 22:34:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
-def reverse_each_word(wrds)
-  rvs_wrds = []
-  sm_strng = wrds.split(" ")
-  sm_strng.each do |wrd|
-    rvs_wrds << wrd.reverse  
-  end
-  return rvs_wrds.join(" ")
-end
+#def reverse_each_word(wrds)
+#  rvs_wrds = []
+#  sm_strng = wrds.split(" ")
+#  sm_strng.each do |wrd|
+#    rvs_wrds << wrd.reverse  
+#  end
+#  return rvs_wrds.join(" ")
+#end
 
-def reverse_each_word2(wrds)
+def reverse_each_word(wrds)
   sm_strng = wrds.split(" ")
   rvs_wrds2 = sm_strng.collect { |wrd|  wrd.reverse}
   return rvs_wrds2.join(" ")
