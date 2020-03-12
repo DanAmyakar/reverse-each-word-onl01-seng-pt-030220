5ca9d193a6df9712d87b4af1d3315ecc1cd5b9b2
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-12 21:43:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,23 +1,23 @@
 # Creating a method that takes a string arg
-def reverse_each_word(wrds)
-  
-  # creates an array from the motified wrds
-  rvs_wrds = []
-  
-  # Creates an array from the string...
-  sm_strng = wrds.split(" ")
-  
-  # Reverses each elements value
-  sm_strng.each do |wrd|
-    
-    #stores them in an array
-    rvs_wrds << wrd.reverse  
-  end
-  
-  #returns and joins our modified wrds with a space between them
-  return rvs_wrds.join(" ")
-
-end
+#def reverse_each_word(wrds)
+#  
+#  # creates an array from the motified wrds
+#  rvs_wrds = []
+#  
+#  # Creates an array from the string...
+#  sm_strng = wrds.split(" ")
+#  
+#  # Reverses each elements value
+#  sm_strng.each do |wrd|
+#    
+#    #stores them in an array
+#    rvs_wrds << wrd.reverse  
+#  end
+#  
+#  #returns and joins our modified wrds with a space between them
+#  return rvs_wrds.join(" ")
+#
+#end
 
 # Create a method that takes a string arg
 def reverse_wrds(wrds)
