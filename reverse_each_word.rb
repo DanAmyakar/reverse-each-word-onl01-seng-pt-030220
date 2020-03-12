f1050801468736bd90e1aa18a25d2d2ea61b35a3
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-12 22:37:13 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+#def reverse_each_word(wrds)
+#  rvs_wrds = []
+#  sm_strng = wrds.split(" ")
+#  sm_strng.each do |wrd|
+#    rvs_wrds << wrd.reverse  
+#  end
+#  return rvs_wrds.join(" ")
+#end
+
+def reverse_each_word(wrds)
+  sm_strng = wrds.split(" ")
+  rvs_wrds2 = sm_strng.collect { |wrd|  wrd.reverse}
+  return rvs_wrds2.join(" ")
+end
+
